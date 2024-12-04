76fc49e526fe673baaa845111f4304a45061f5bb
JNC <nilimajnc@gmail.com>
2024-12-05 04:16:47 +0530

    city wise hotspots added

---
+++ backend/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+import numpy as np
+from typing import List, Dict
+
+# Import routes for hotspots
+from routes.hotspotRoutes import router as hotspot_router
+
+app = FastAPI()
+
+# Enable CORS for frontend React app
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow any domain, or specify the frontend domain
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Include hotspot routes
+app.include_router(hotspot_router,prefix="/hotspot")
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Crime Hotspot API!"}
